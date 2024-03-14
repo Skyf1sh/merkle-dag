27ed5e96a38f71bce8970bc3158cfa65ad70d949
Skyf1sh <273005781@qq.com>
2024-03-14 20:23:37 +0800

    实验

--- dag.go
+++ dag.go
@@ -1,8 +1,45 @@
 package merkledag
 
-import "hash"
+import (
+	"hash"
+)
 
 func Add(store KVStore, node Node, h hash.Hash) []byte {
-	// TODO 将分片写入到KVStore中，并返回Merkle Root
-	return nil
+	switch node.Type() {
+	case FILE:
+		fileNode := node.(File)
+		data := fileNode.Bytes()
+		hash := HashData(data, h)
+		if err := store.Put(hash, data); err != nil {
+			panic(err) // Handle error appropriately
+		}
+		return hash
+	case DIR:
+		dirNode := node.(Dir)
+		it := dirNode.It()
+		var childrenHashes [][]byte
+		for it.Next() {
+			childNode := it.Node()
+			childHash := Add(store, childNode, h)
+			childrenHashes = append(childrenHashes, childHash)
+		}
+		hash := HashChildren(childrenHashes, h)
+		return hash
+	default:
+		panic("Unknown node type")
+	}
+}
+
+func HashData(data []byte, h hash.Hash) []byte {
+	h.Reset()
+	h.Write(data)
+	return h.Sum(nil)
+}
+
+func HashChildren(children [][]byte, h hash.Hash) []byte {
+	h.Reset()
+	for _, child := range children {
+		h.Write(child)
+	}
+	return h.Sum(nil)
 }
